ec1c4e5f5e17ba482bd1f699cb14e1d0c1ff4a23
Sam5196 <sammedpachore1008@gmail.com>
2022-08-22 21:34:42 +0530

    Added:DataBase connect to java sucessfully

--- src/com/bridgelabz/EmployeePayrollJDBC.java
+++ src/com/bridgelabz/EmployeePayrollJDBC.java
@@ -1,8 +1,21 @@
 package com.bridgelabz;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class EmployeePayrollJDBC {
-    public static void main(String[] args) {
-            System.out.println("Welcome to Employee Payroll program on JDBC");
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Welcome to Employee Payroll program on JDBC");
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root", "Sammed@10");
+            System.out.println("Connection done.....");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
     }
 }
 
