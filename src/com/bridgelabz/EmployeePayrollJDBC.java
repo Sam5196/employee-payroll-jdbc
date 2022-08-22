841afd61dfb6d52ddb50178e66c247789a18a148
Sam5196 <sammedpachore1008@gmail.com>
2022-08-22 21:32:17 +0530

    Welcome to Employee Payroll Program on JDBC

---
+++ src/com/bridgelabz/EmployeePayrollJDBC.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class EmployeePayrollJDBC {
+    public static void main(String[] args) {
+            System.out.println("Welcome to Employee Payroll program on JDBC");
+    }
+}
+
