a082c0161a547ecbf93656d7893d672d0552fbda
Sam5196 <sammedpachore1008@gmail.com>
2022-08-23 09:07:38 +0530

    Added:Retrieve the data form the table using prepared statements by name

--- src/com/bridgelabz/EmployeePayrollJDBC.java
+++ src/com/bridgelabz/EmployeePayrollJDBC.java
@@ -6,15 +6,16 @@ public class EmployeePayrollJDBC {
         System.out.println("Welcome to Employee Payroll program on JDBC");
         double BasicPay = 4000000.00;
         int id = 1;
+        String sql = "SELECT name FROM employee_payroll";
         Connection connection = null;
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root", "Sammed@10");
             System.out.println("Connection done.....");
-            String sql = "UPDATE employee_payroll SET BasicPay=? WHERE id= ?";
-            PreparedStatement statement1 = connection.prepareStatement(sql);
-            statement1.setDouble(1, BasicPay);
-            statement1.setInt(2, id);
-            statement1.executeUpdate();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
