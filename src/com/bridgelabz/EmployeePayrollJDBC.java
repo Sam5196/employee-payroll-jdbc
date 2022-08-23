078fc1ed24734b586dce5374426ed7ddb3ca4cee
Sam5196 <sammedpachore1008@gmail.com>
2022-08-23 09:10:35 +0530

    Added:Inserting the new contcat to table using JDBC

--- src/com/bridgelabz/EmployeePayrollJDBC.java
+++ src/com/bridgelabz/EmployeePayrollJDBC.java
@@ -4,17 +4,26 @@ import java.sql.*;
 public class EmployeePayrollJDBC {
     public static void main(String[] args) throws SQLException {
         System.out.println("Welcome to Employee Payroll program on JDBC");
-        double BasicPay = 4000000.00;
-        int id = 1;
-        String sql = "SELECT name FROM employee_payroll";
         Connection connection = null;
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root", "Sammed@10");
             System.out.println("Connection done.....");
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("INSERT INTO employee_payroll VALUES (5,'John','8919584625','BKS','Sales','M',100000.00,20000.00,80000.00,92000.00,8000.00,'2021-03-01')");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_payroll");
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("name"));
+                System.out.println(resultSet.getInt(1)
+                        + " " + resultSet.getString(2)
+                        + " " + resultSet.getString(3)
+                        + " " + resultSet.getString(4)
+                        + " " + resultSet.getString(5)
+                        + " " + resultSet.getString(6)
+                        + " " + resultSet.getDouble(7)
+                        + " " + resultSet.getDouble(8)
+                        + " " + resultSet.getDouble(9)
+                        + " " + resultSet.getDouble(10)
+                        + " " + resultSet.getDouble(11)
+                        + " " + resultSet.getDate(12));
             }
         } catch (Exception e) {
             e.printStackTrace();
